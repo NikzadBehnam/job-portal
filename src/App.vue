2ec682ab6931827f4dbed637e99fff0534a2c4d5
n.behnam <n.behnam@abacogroup.eu>
2023-08-30 15:55:08 +0200

    47. Rendering Data to View I- Adding : dadaproject logo on header

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,4 @@ export default {
   }
 }
 </script>
-<style lang="">
-  
-</style>
+<style lang=""></style>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -3,7 +3,13 @@
     <h1 class="text-3xl font-bold underline">
       <div class="fixed top-0 left-0 w-full h-16 bg-white">
         <div class="flex h-full px-8 mx-auto border-b border-solid flex-nowrap border-brand-gray-1">
-          <a class="flex items-center h-full text-xl" href="/">BoProject</a>
+          <a class="flex items-center h-full text-2xl font-thin" href="/">
+            <div class="pr-1 m-0 uppercase border border-brand-green-1">
+              <span class="font-normal text-white bg-brand-green-1">{{
+                companyName.slice(0, 4)
+              }}</span>{{ companyName.slice(4) }}
+            </div>
+          </a>
         </div>
       </div>
     </h1>
@@ -11,6 +17,11 @@
 </template>
 <script>
 export default {
-  name: "MainNav"
+  name: 'MainNav',
+  data() {
+    return {
+      companyName: 'dadaproject'
+    }
+  }
 }
 </script>
