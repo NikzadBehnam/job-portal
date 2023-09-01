24fc2dcefc00cc84545e989de55178aed21eb9e6
n.behnam <n.behnam@abacogroup.eu>
2023-09-01 19:21:20 +0200

    94. The userEvent Library

--- tests/unit/MainNav.test.js
+++ tests/unit/MainNav.test.js
@@ -1,6 +1,6 @@
 import { screen, render } from '@testing-library/vue'
+import userEvent from '@testing-library/user-event'
 import MainNav from '@/components/MainNav.vue'
-import { expect } from 'vitest'
 
 describe('MainNav', () => {
   // a single unit test
@@ -27,12 +27,22 @@ describe('MainNav', () => {
   // New test title
   describe('when the user logs in', () => {
     // a single unit test
-    it('displays user profile picture', () => {
+    it('displays user profile picture', async () => {
       render(MainNav)
-      const profileImage = screen.queryByRole('img', {
+      let profileImage = screen.queryByRole('img', {
         name: /user profile image/i
       })
       expect(profileImage).not.toBeInTheDocument()
+
+      const loginButton = screen.getByRole('button', {
+        name: /sign in/i
+      })
+
+      await userEvent.click(loginButton)
+      profileImage = screen.queryByRole('img', {
+        name: /user profile image/i
+      })
+      expect(profileImage).toBeInTheDocument()
     })
   })
 })
