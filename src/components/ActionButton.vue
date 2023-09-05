d733eca198adf45b542b9f748572c004ad6d9b53
n.behnam <n.behnam@abacogroup.eu>
2023-09-05 18:11:58 +0200

    112. Updating Prop Validation in ActionButton

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -4,12 +4,21 @@
 <script>
 export default {
   name: 'ActionButton',
-  props: ['text', 'type'],
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: false,
+      default: "primary"
+    }
+  },
   computed: {
     buttonClass() {
       return {
-        primary: this.type === "primary",
-        secondary: this.type === "secondary"
+        [this.type]: true
       }
     }
   }
