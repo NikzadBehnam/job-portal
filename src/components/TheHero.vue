5485025a29e84a3b2cc81dfc4b0ec1aeb083299f
n.behnam <n.behnam@abacogroup.eu>
2023-09-08 12:47:16 +0200

    133. Apply CSS Grid to TheHero

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,6 +1,21 @@
 <template>
   <main>
-    <section class="flex flex-col h-screen pt-10 pb-20 bg-yellow-200">This is the hero</section>
+    <section class="flex flex-col h-screen pt-10 pb-20 bg-yellow-200">
+      <div class="grid grid-cols-12 ">
+        <div class="col-span-1 col-start-1 border border-gray-700">
+          1 col
+        </div>
+        <div class="col-span-5 col-start-2 border border-gray-700">
+          2 col
+        </div>
+        <div class="col-span-5 col-start-7 border border-gray-700">
+          3 col
+        </div>
+        <div class="col-span-1 col-start-12 border border-gray-700">
+          4 col
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 <script>
