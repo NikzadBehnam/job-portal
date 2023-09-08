c7c5ae0399182d0147ec95286a9a81c20c97c603
n.behnam <n.behnam@abacogroup.eu>
2023-09-08 14:06:02 +0200

    137. Changing the Action Verb in TheHeadline

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -1,6 +1,6 @@
 <template >
   <section>
-    <h1>Lorem ipsum dolor sit.</h1>
+    <h1>{{ action }} ipsum dolor sit.</h1>
     <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, quas?.</h2>
   </section>
 </template>
@@ -9,11 +9,27 @@ export default {
   name: "TheHeadline",
   data() {
     return {
-      sample: "Hello"
+      action: "Build",
+      interval: null
     }
   },
+  methods: {
+    changeTitle() {
+      this.interval = setInterval(() => {
+        const actions = ["Build", "Create", "Design", "Code"]
+        const curActionIndex = actions.indexOf(this.action)
+        const nextActionIndex = (curActionIndex + 1) % 4; // 4 % 4 is 0
+        const nextAction = actions[nextActionIndex]
+        this.action = nextAction
+
+      }, 3000);
+    }
+  },
+  beforeUnmount() {
+    clearInterval(this.interval)
+  },
   created() {
-    console.log("Hey I am just created and my data", this.sample);
+    this.changeTitle()
   }
 }
 </script>
