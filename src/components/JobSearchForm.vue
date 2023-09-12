052e43ab07fa48cc52cc0ad855937f6036e4e202
n.behnam <n.behnam@abacogroup.eu>
2023-09-12 13:56:10 +0200

    162. Two-Way Data Binding with v-model Directive

--- src/components/JobSearchForm.vue
+++ src/components/JobSearchForm.vue
@@ -2,15 +2,24 @@
   <form action="" class="flex items-center w-full h-12 border border-solid rounded-3xl border-brand-gray-3 ">
     <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
     <div class="flex flex-1 h-full text-base font-light flex-nowrap ">
+
+      <!-- Roll Input -->
       <div class="relative flex flex-1 h-full pr-3 iteme-center">
         <label for="role" class="absolute left-0 -top-10 ">Role</label>
-        <input type="text" placeholder="Software engineer" class="w-full text-lg font-normal focus:outline-none">
+
+        <input v-model="role" type="text" placeholder="Software engineer"
+          class="w-full text-lg font-normal focus:outline-none">
       </div>
+
+      <!-- location Input -->
       <span class="flex items-center h-full px-3 border-l border-r border-brand-gray-3 bg-brand-gray-2">in</span>
       <div class="relative flex flex-1 h-full pl-3 iteme-center">
-        <label for="role" class="absolute left-0 -top-10 ">Where?</label>
-        <input type="text" placeholder="Los Angeles" class="w-full text-lg font-normal focus:outline-none">
+        <label for="location" class="absolute left-0 -top-10 ">Where?</label>
+
+        <input v-model="location" type="text" placeholder="Los Angeles"
+          class="w-full text-lg font-normal focus:outline-none">
       </div>
+
     </div>
     <action-button text="search" type="secondary" class="rounded-r-3xl" />
   </form>
@@ -23,6 +32,13 @@ export default {
   name: "JobSearchForm",
   components: {
     ActionButton
-  }
+  },
+  data() {
+    return {
+      role: "",
+      location: "",
+    };
+  },
+
 }
 </script>
