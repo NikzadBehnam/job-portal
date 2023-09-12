d95bda5a99e0c0748b489fe68d3606b2525aba85
n.behnam <n.behnam@abacogroup.eu>
2023-09-12 17:55:17 +0200

    170. Creating Reusable TextInput Component

---
+++ src/components/Shared/TextInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <input type="text" :value="value" @input="handleInput" class="w-full text-lg font-normal focus:outline-none">
+</template>
+<script>
+export default {
+  name: "TextInput",
+  data() {
+    return {
+      value: ""
+    }
+  },
+  methods: {
+    handleInput($event) {
+      this.value = $event.target.value
+    }
+  }
+}
+</script>
